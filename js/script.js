27d5eeb9a743abb483a202351f615cf26316fe9b
Joenathan Sirait <siraitjoenathan@gmail.com>
2022-10-13 13:26:48 +0700

    90%

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function hideSidebar() {
     const sidebar = document.getElementById("sidebar");
     const profilesidebar = document.getElementById("profile-sidebar")
+    const actionsidebar = document.getElementById("action-sidebar")
     const main = document.getElementById("main")
-    if (sidebar.classList.contains("col-lg-1") || profilesidebar.classList.contains("d-none")) {
+    if (sidebar.classList.contains("col-lg-1") || profilesidebar.classList.contains("d-none") ||  actionsidebar.classList.contains("d-none")) {
         main.classList.add("col-lg-10")
         sidebar.classList.add("col-lg-2");
         profilesidebar.classList.remove("d-none")
+        actionsidebar.classList.remove("d-none")
         sidebar.classList.remove("col-lg-1");
         main.classList.remove("col-lg-11");
       
@@ -13,6 +15,7 @@ function hideSidebar() {
         sidebar.classList.remove("col-lg-2")
         main.classList.remove("col-lg-10")
         profilesidebar.classList.add("d-none")
+        actionsidebar.classList.add("d-none")
         sidebar.classList.add("col-lg-1");
         main.classList.add("col-lg-11");
       
